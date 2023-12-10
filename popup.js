0ef1f2e74e7040077b3efa66b404f63fb4b2a09c
西西里卷 <zqbinary@foxmail.com>
2023-12-11 00:21:55 +0800

    记住上一次

--- popup.js
+++ popup.js
@@ -3,12 +3,14 @@ function sendMsg() {
     if (!msg) {
         return
     }
-    chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-        if (tabs.length === 0) return;
-        chrome.tabs.sendMessage(tabs[0].id, {action: 'selectCss', value: msg}, () => {
+    chrome.storage.sync.set({'qSelecor': msg}, () => {
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+            if (tabs.length === 0) return;
+            chrome.tabs.sendMessage(tabs[0].id, {action: 'selectCss', value: msg}, () => {
 
+            });
         });
-    });
+    })
 }
 
 document.querySelector('#btn').addEventListener('click', async (m) => {
@@ -17,3 +19,15 @@ document.querySelector('#btn').addEventListener('click', async (m) => {
 document.querySelector('form').addEventListener('submit', async (m) => {
     sendMsg()
 })
+// 等待页面加载完成后执行代码
+document.addEventListener('DOMContentLoaded', function () {
+    chrome.storage.sync.get('qSelecor', data => {
+        let res = data['qSelecor']
+        console.log('iii', data, res)
+        if (data && res) {
+            document.querySelector('#value').value = res;
+        } else {
+            document.querySelector('#value').value = '';
+        }
+    })
+});
